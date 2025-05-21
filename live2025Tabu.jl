9a7932296d4c2fe5f2442fff6a1d0aab5ce3f4eb
mdestr <manuel.destremon@gmail.com>
2025-05-22 01:29:33 +0200

    Maj Julia tsp problem with tabous

--- live2025Tabu.jl
+++ live2025Tabu.jl
@@ -1,37 +1,59 @@
 # www.jdoodle.com/online-compiler-julia
+
+# Fonction d'évaluation
 function eval(s)
+    #= Prend en entrée un chemin s (ex. [1, 3, 5, ..., 1])
+    calcule sa longueur totale (somme des distances entre les villes consécutives).
+    La matrice d contient les distances entre les villes.
+    On considère que la ville de départ est aussi celle d'arrivée (cycle fermé). =#
     dist = 0;
     for i in 1:n
         dist = dist + d[s[i],s[i+1]];
     end
     return dist;
 end
-function tsp()  #traveling salesman problem 
-    s=Vector(1:n)
-    s=vcat(s,1);
-    s=reverse(s,Int(n/2),n);
-    tabu = -Inf*ones(n);
+
+# Fonction principale
+function tsp()  # traveling salesman problem 
+
+    # initialisation de la solution de départ
+    s=Vector(1:n)               # solution de base : [1, 2,... n]
+    s=vcat(s,1);                # on boucle avec un retour à la ville de départ :  [1, 2,... n, 1]
+    s=reverse(s,Int(n/2),n);    # on inverse la 2e moitié du voyage afin de générer une solution de base aléatoire
+    tabu = -Inf*ones(n);        # on intitialise la liste taboue
+                                # tableau dans lequel on enregistre à quelle itération chaque index a été modifié
+                                # tabu[i]=iter signifie : la ville i a été modifiée à l'itération iter
     println(s);
-    bstObj = eval(s);
+    bstObj = eval(s);           # sauvegarde le coût de la solution initiale
+                                # par la suite bstObj stockera la meilleure oslution courante
     println(" sol=",s," de cout ",bstObj);
+
+    # On effectue 20 itérations maximum
     maxIter = 20;
     for iter in 1:maxIter
         #println("Iteration ",iter);
         distInit = eval(s);
         distNew  = Inf;
         sNew     = s;
-        #cette boucle fait l'évaluation du voisinage (toutes les transformations dispo)
+
+        # Cette boucle fait l'évaluation du voisinage (toutes les transformations dispo).
+        # On explore tous les échanges possibles entre 2 villes i et j 
+        # sauf les villes de départ et arrivée.
         for i in 2:n-1
           for j in i+1:n
-            #on essaye d'échanger i avec j
+            # On essaye d'échanger i avec j seulement si les 2 indices
+            # ne sont pas tabous depuis moins de 6 itérations.
             if tabu[i]<iter-6 && tabu[j]<iter-6
+                # Si l'échange est autorisé on permute les 2 villes sur une copie de la solution courante
+ 
                 s2      = copy(s);
                 s2[i]   = s[j];
-                s2[j] = s[i];
+                s2[j]   = s[i];
                 #println(" j'essaye sol=",s2," de cout ",eval(s2));
                 if(eval(s2)<=distNew)
+                    # on évalue le nouveau chemin
                     distNew = eval(s2);
-                    sNew = s2;
+                    sNew = s2;  # on conserve le meilleur voisin admissible de cette itération
                     if distNew<bstObj
                         bstObj = distNew;
                     end
@@ -39,22 +61,21 @@ function tsp()  #traveling salesman problem
             end
           end
         end
+        # On met à jour la liste taboue
         for i = 2:n
+            # les villes modifiées entre s et sNew deviennent taboues pour 6 itérations
             if s[i]!=sNew[i]
                 tabu[i] = iter;
             end
         end
+        # sNew devient la solution courante pour la prochaine itération
         s = sNew;
         println(" sol=",s," de cout ",distNew);
-        #if(distNew==distInit)
-        #    println(" Minimum local trouvé. Stop");
-        #    return distInit;
-        #end
     end
     return bstObj;
 end
 #le programme principal
-n = 22;
+n = 8;
 d = zeros(Int, n,n);
 for i in 1:n
     for j in 1:n
--- tsp_recuit_simule.py
+++ tsp_recuit_simule.py
@@ -6,18 +6,72 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _():
-    return
-
-
-@app.cell
-def _():
     import numpy as np
     import matplotlib.pyplot as plt
     import random as rd
+    return np, plt
 
+
+@app.cell
+def _(np, plt):
     # Paramètres
-    N = 20  # Nombre de points à générer
+    # Nombre de villes (points à générer)
+    N = 5
+    # La température initiale doit être élevée
+    T0 = 15
+    T = T0
+    # Température minimum pour arrêter le recuit
+    #Tmin = 1e-2
+    Tmin = 9
+    # Paramètre pour calculer la décroissance de la température
+    # tau doit être assez grand pour que le refroidissement soit suffisamment lent, 
+    # mais pas trop pour ne pas avoir trop de calcul
+    tau = 1e2
+    # Probabilité d'accepter ou non la solution
+    beta = 1e-1
+    k=0
+    t=0
 
+    # calcul de la fonction coût
+    def fonction_cout():
+        global chemin
+        energie = 0.0
+        #xy = np.concatenate((x[chemin], y[chemin]))
+        xy = np.column_stack((x[chemin], y[chemin]))
+        print('x = ', x)
+        print('y = ', y)
+        print('coordonnées x et y : ', xy)
+        energie = np.sum(np.sqrt(np.sum((xy -np.roll(xy,-1,axis=0))**2,axis=1)))
+        return energie
+
+    def deplacement(ville1, ville2):
+        # déplacement aléatoire
+        global chemin
+        Min = min(ville1, ville2)
+        Max = max(ville1, ville2)
+        chemin[Min:Max] = chemin[Min:Max].copy()[::-1]
+        return
+    
+
+
+    # choix du chemin initial
+    x=np.random.uniform(0,1,N)
+    y=np.random.uniform(0,1,N)
+    chemin = np.arange(N)
+
+    cout = fonction_cout()
+
+    print('k = ', k, 'Chemin Hamiltonien : ', chemin)
+    print('Coût du chemin = Distance du chemin = ', cout)
+
+    while k<N:
+        plt.plot(x, y, x[chemin], y[chemin], marker='o', color='k')
+        k+=1
+
+    # Affichage final
+    plt.show()
+
+    '''
     # Initialisation du graphique
     plt.figure(figsize=(8, 8))
     plt.title(f"Génération de {N} points aléatoires")
@@ -30,9 +84,7 @@ def _():
         x = rd.uniform(0, 1)
         y = rd.uniform(0, 1)
         plt.plot(x, y, marker='o', color='r', markersize=8)
-
-    # Affichage final
-    plt.show()
+    '''
     return
 
 
